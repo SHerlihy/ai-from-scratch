ddc9fe8304b90f76822afc2022f3bb4e4beb7796
Steven Herlihy <steven_herlihy@yahoo.com>
2026-07-27 19:07:55 +0100

    take sliding window sample

--- main.py
+++ main.py
@@ -1,35 +1,13 @@
-import re
+import tiktoken
 
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
-preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
-preprocessed = [item.strip() for item in preprocessed if item.strip()]
+tokenizer = tiktoken.get_encoding("gpt2")
+enc_sample = tokenizer.encode(raw_text)[50:]
 
-all_tokens = sorted(list(set(preprocessed)))
-all_tokens.extend(["<|endoftext|>", "<|unk|>"])
-vocab = {token:integer for integer,token in enumerate(all_tokens)}
-
-class SimpleTokenizerV2:
-    def __init__(self, vocab):
-        self.str_to_int = vocab
-        self.int_to_str = {i:s for s, i in vocab.items()}
-
-    def encode(self, text):
-        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
-        preprocessed = [
-                item.strip() for item in preprocessed if item.strip()
-            ]
-        preprocessed = [
-                item if item in self.str_to_int
-                else "<|unk|>" for item in preprocessed
-            ]
-        ids = [self.str_to_int[s] for s in preprocessed]
-        return ids
-
-    def decode(self, ids):
-        text = " ".join([self.int_to_str[i] for i in ids])
-
-        text = re.sub(r"\s+([,.?!”\"()\'])", r"\1", text)
-        text = re.sub(r"'\s+([a-zA-Z])", r"'\1", text)
-        return text
+context_size = 4
+x = enc_sample[:context_size]
+y = enc_sample[1:context_size+1]
+print(f"x: {x}")
+print(f"y:      {y}")
