86de4f7122def9ff827ea9e2884beb72154e108c
Steven Herlihy <steven_herlihy@yahoo.com>
2026-07-28 09:09:45 +0100

    add CausalAttention

--- ai_from_scratch/model/__init__.py
+++ ai_from_scratch/model/__init__.py
@@ -1,2 +1,5 @@
-"""Model components."""
+from ai_from_scratch.model.model import CausalAttention
+
+__all__ = ["CausalAttention"]
+
 
---
+++ tests/model/test_model.py
@@ -0,0 +1,23 @@
+import unittest
+from unittest.mock import patch
+
+import torch
+
+from ai_from_scratch.model import CausalAttention
+from ai_from_scratch.data import GPTDatasetV1, create_dataloader_v1
+
+class CausalAttentionTests(unittest.TestCase):
+    def test_vector_shape(self):
+        inputs =  
+
+        batch = torch.stack((inputs, inputs), dim=0)
+
+        torch.manual_seed(123)
+        context_length = batch.shape[1]
+        ca = CausalAttention(d_in, d_out, context_length, 0.0)
+        context_vecs = ca(batch)
+
+        self.assertEqual(context_vecs.shape, torch.Size([2, 6, 2]))
+
+if __name__ == "__main__":
+    unittest.main()
